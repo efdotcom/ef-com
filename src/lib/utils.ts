9ab8928903b6e499a492f99bbb3a7da820421806
Joshua Robson <joshua.robson@riskworx.com>
2026-03-22 14:22:14 +0200

    test

--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export function cn(...inputs: (string | undefined | null | false)[]) {
-  return inputs.filter(Boolean).join(' ');
-}
-
-// DO NOT CHANGE THIS IT WILL BREAK SOME OF THE CSS COMPONENTS
