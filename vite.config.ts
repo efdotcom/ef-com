f0deba2be0597cca3f2672b18bad8ab88472d60f
Joshua Robson <joshua.robson@riskworx.com>
2026-03-22 16:05:35 +0200

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -10,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/repository-name/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
